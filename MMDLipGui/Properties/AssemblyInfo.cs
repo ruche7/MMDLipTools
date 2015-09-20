7e0a07a2ef210b8f428245020c4134a4d2b1a447
ruche7 <pg@ruche-home.net>
2015-09-21 06:35:01 +0900

    ILMerge のためにWPF周りを MMDLipGui アセンブリに分離。

--- MMDLipGui/Properties/AssemblyInfo.cs
+++ MMDLipGui/Properties/AssemblyInfo.cs
@@ -1,14 +1,15 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Windows.Markup;
 
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("MMDLipCore")]
-[assembly: AssemblyDescription("Core API for the lip-morph of MMD models.")]
+[assembly: AssemblyTitle("MMDLipGui")]
+[assembly: AssemblyDescription("GUI library for the lip-morph of MMD models.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ruche-home")]
-[assembly: AssemblyProduct("MMDLipCore")]
+[assembly: AssemblyProduct("MMDLipGui")]
 [assembly: AssemblyCopyright("Copyright (C) ruche.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
-[assembly: Guid("1e9afffd-e67a-4e18-b1bb-7c661c1b7632")]
+[assembly: Guid("5246b42b-96f2-485b-8281-b062e9615d93")]
 
 // アセンブリのバージョン情報は次の 4 つの値で構成されています:
 //
@@ -32,3 +33,8 @@ using System.Runtime.InteropServices;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.*")]
+
+// XML名前空間
+[assembly: XmlnsDefinition("http://schemas.ruche-home.net/xaml/mmd/gui/lip", "ruche.mmd.gui.lip")]
+[assembly: XmlnsDefinition("http://schemas.ruche-home.net/xaml/wpf", "ruche.wpf")]
+[assembly: XmlnsDefinition("http://schemas.ruche-home.net/xaml/wpf/converters", "ruche.wpf.converters")]
